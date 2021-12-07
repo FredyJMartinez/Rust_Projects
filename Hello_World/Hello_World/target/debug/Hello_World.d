1e3b71a7c6944213863db44cf0b3fc1ece264e26
fredy <fredyjmartinez99@users.noreply.github.com>
2021-12-07 17:47:12 -0600

    testing recommit

---
+++ Hello_World/Hello_World/target/debug/Hello_World.d
@@ -0,0 +1,1 @@
+/home/fredy/src/Rust_Projects/Hello_World/Hello_World/target/debug/Hello_World: /home/fredy/src/Rust_Projects/Hello_World/Hello_World/src/main.rs
---
+++ Hello_World/Hello_World/target/debug/deps/Hello_World-662867ba2fab2674.d
@@ -0,0 +1,5 @@
+/home/fredy/src/Rust_Projects/Hello_World/Hello_World/target/debug/deps/Hello_World-662867ba2fab2674: src/main.rs
+
+/home/fredy/src/Rust_Projects/Hello_World/Hello_World/target/debug/deps/Hello_World-662867ba2fab2674.d: src/main.rs
+
+src/main.rs:
