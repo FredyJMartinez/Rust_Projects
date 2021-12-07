ea1d791b590b2be6b324c2fc807b784824aff51a
fredy <fredyjmartinez99@users.noreply.github.com>
2021-12-07 17:44:26 -0600

    Adds Hello_World project

---
+++ Hello_World/Hello_World/target/debug/deps/Hello_World-1517aa37d096a145.d
@@ -0,0 +1,5 @@
+/home/fredy/src/Rust_Projects/Hello_World/Hello_World/target/debug/deps/Hello_World-1517aa37d096a145.rmeta: src/main.rs
+
+/home/fredy/src/Rust_Projects/Hello_World/Hello_World/target/debug/deps/Hello_World-1517aa37d096a145.d: src/main.rs
+
+src/main.rs:
---
+++ Hello_World/Hello_World/target/debug/deps/Hello_World-4e3a5efdd5f77ac4.d
@@ -0,0 +1,5 @@
+/home/fredy/src/Rust_Projects/Hello_World/Hello_World/target/debug/deps/Hello_World-4e3a5efdd5f77ac4.rmeta: src/main.rs
+
+/home/fredy/src/Rust_Projects/Hello_World/Hello_World/target/debug/deps/Hello_World-4e3a5efdd5f77ac4.d: src/main.rs
+
+src/main.rs:
