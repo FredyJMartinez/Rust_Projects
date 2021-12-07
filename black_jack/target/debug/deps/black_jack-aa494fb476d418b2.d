8716af51c38760e939dc050c88799283432f47d0
fredy <fredyjmartinez99@users.noreply.github.com>
2021-12-07 17:49:34 -0600

    Create black_jack project

---
+++ black_jack/target/debug/deps/black_jack-aa494fb476d418b2.d
@@ -0,0 +1,5 @@
+/home/fredy/src/Rust_Projects/black_jack/target/debug/deps/black_jack-aa494fb476d418b2.rmeta: src/main.rs
+
+/home/fredy/src/Rust_Projects/black_jack/target/debug/deps/black_jack-aa494fb476d418b2.d: src/main.rs
+
+src/main.rs:
---
+++ black_jack/target/debug/deps/black_jack-d66a28065dc23591.d
@@ -0,0 +1,5 @@
+/home/fredy/src/Rust_Projects/black_jack/target/debug/deps/black_jack-d66a28065dc23591.rmeta: src/main.rs
+
+/home/fredy/src/Rust_Projects/black_jack/target/debug/deps/black_jack-d66a28065dc23591.d: src/main.rs
+
+src/main.rs:
